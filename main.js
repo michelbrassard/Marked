ec6254aa5c4e8c950ae11bb9eb27c2853d3a31d9
Michel Brassard <mb6149@rit.edu>
2025-02-03 19:47:01 +0100

    add dark mode support

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain, nativeTheme, Menu } = require('electron')
 const path = require('node:path')
 
 const createWindow = () => {
@@ -12,9 +12,41 @@ const createWindow = () => {
     })
   
     window.loadFile('index.html')
-  }
+}
+
+const dockMenu = Menu.buildFromTemplate([
+    {
+      label: 'New Window',
+      click () { console.log('New Window') }
+    }, {
+      label: 'New Window with Settings',
+      submenu: [
+        { label: 'Basic' },
+        { label: 'Pro' }
+      ]
+    },
+    { label: 'New Command...' }
+])
+
+ipcMain.handle('dark-mode:toggle', () => {
+    if (nativeTheme.shouldUseDarkColors) {
+      nativeTheme.themeSource = 'light'
+    } else {
+      nativeTheme.themeSource = 'dark'
+    }
+    return nativeTheme.shouldUseDarkColors
+  })
+  
+  ipcMain.handle('dark-mode:system', () => {
+    nativeTheme.themeSource = 'system'
+  })
+  
 
 app.whenReady().then(() => {
+    // eslint-disable-next-line no-undef
+    if (process.platform === 'darwin') {
+        app.dock.setMenu(dockMenu)
+      }
     createWindow()
     
     //On Mac, when all windows are closed it doesn't mean the app is closed
